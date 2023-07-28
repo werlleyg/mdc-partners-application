1c99aafdacf955068904500f34bad40f1b86f622
Werlley Gonçalves <werlley.registros@hotmail.com>
2023-07-28 11:58:11 -0300

    feat: add spinner in simplecalculator page

--- src/pages/simplecalculator/index.tsx
+++ src/pages/simplecalculator/index.tsx
@@ -7,6 +7,7 @@ import { Container, DivResult, Form, P, Span, Subtitle } from "./styles";
 import { CustomHead } from "@/layout/CustomHead";
 import { H1 } from "@/components/H1";
 import { InputCustom } from "@/components/Input/styles";
+import { DotSpinner } from "@/components/DotSpinner";
 // types
 import { ICalculator } from "@/dtos/calculator";
 import { Button } from "@/components/Button";
@@ -16,6 +17,8 @@ export default function SimpleCalculator() {
   const [titlePage] = useState<string>("Simple Calculator");
   const [calculatorData, setCalculatorData] = useState<ICalculator>();
   const [calculationResult, setCalculationResult] = useState<ICalculator>();
+  const [showSpinner, setShowSpinner] = useState<boolean>(false);
+  const DELAY_TIME: number = 3; // in seconds
 
   // handle change input
   const handleChangeInput = useCallback(
@@ -69,12 +72,22 @@ export default function SimpleCalculator() {
         return toast.error("Calculation error");
     }
 
-    setCalculationResult({ ...calculatorData, result });
+    // set calculate result and show on screen
+    setTimeout(() => {
+      setCalculationResult({ ...calculatorData, result });
+      // hide spinner
+      setShowSpinner(false);
+    }, DELAY_TIME * 1000);
   }, [calculatorData]);
 
   const handleSubmit = useCallback(
     (event: FormEvent<HTMLFormElement>) => {
       event.preventDefault();
+      // show spinner
+      setShowSpinner(true);
+      // hide show result
+      setCalculationResult({ ...calculatorData, result: undefined });
+      // set calculator
       calculator();
     },
     [calculator],
@@ -127,6 +140,7 @@ export default function SimpleCalculator() {
             <Span>{calculationResult?.result}</Span>
           </DivResult>
         )}
+        {showSpinner && <DotSpinner />}
       </Container>
     </>
   );
