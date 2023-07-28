48e8ff2154c4ff7989599cbd58a8b2d5cf6e8867
Werlley Gonçalves <werlley.registros@hotmail.com>
2023-07-28 17:27:40 -0300

    feat: create vowelcounter page layout and functions

---
+++ src/pages/vowelcounter/styles.ts
@@ -0,0 +1,102 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.main`
+  text-align: center;
+  gap: 1rem;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Subtitle = styled.p`
+  color: var(--subtitle-color);
+  width: min(100%, 32rem);
+  margin-inline: auto;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  margin-top: 2rem;
+`;
+
+export const DivDeck = styled.div`
+  padding-top: 1rem;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+  margin: 0 auto;
+  width: min(100%, 40rem);
+  align-items: center;
+  justify-content: center;
+`;
+
+export const DivResultCard = styled.div`
+  background-color: var(--white-color);
+  min-width: 7rem;
+  color: var(--primary-color);
+  text-align: center;
+  padding: 1.25rem;
+  font-weight: 600;
+  box-shadow: 0rem 0.5rem 2.5rem rgba(var(--dark-rgb-color), 0.1);
+
+  animation: floatingAnimation var(--transition-time) forwards;
+
+  @keyframes floatingAnimation {
+    0% {
+      opacity: 0;
+      transform: translateY(20%);
+    }
+    100% {
+      opacity: 1;
+      transform: translateY(0%);
+    }
+  }
+`;
+
+interface IDivResult {
+  showContent?: boolean;
+}
+
+export const DivResult = styled.div<IDivResult>`
+  overflow: hidden;
+  max-height: ${(props) => (props.showContent ? "auto" : "0rem")};
+  padding: ${(props) => (props.showContent ? "1rem 0rem" : "0rem")};
+
+  transition: height ease-in-out 3s;
+  b {
+    background-color: rgba(var(--dark-rgb-color), 0.1);
+    padding: 0rem 0.25rem;
+    border-radius: 0.2rem;
+  }
+  span {
+    font-size: 2rem;
+    font-weight: 600;
+  }
+  .span__is-prime {
+    color: var(--success-color);
+  }
+  .span__not-prime {
+    color: var(--error-color);
+  }
+`;
+export const P = styled.p`
+  margin: 0rem;
+  padding: 0rem;
+
+  b {
+    background-color: rgba(var(--dark-rgb-color), 0.1);
+    padding: 0rem 0.25rem;
+    border-radius: 0.2rem;
+  }
+`;
+export const Span = styled.span`
+  font-size: 3rem;
+  font-weight: 800;
+  margin-inline: auto;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: var(--primary-color);
+`;
