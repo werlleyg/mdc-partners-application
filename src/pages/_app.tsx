28cf7b78e450f6e6fef1d983061c09626a3fcd6c
Werlley Gonçalves <werlley.registros@hotmail.com>
2023-07-29 02:11:41 -0300

    hotfix: clarity token

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -35,7 +35,7 @@ export default function App({ Component, pageProps }: AppProps) {
             c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
             t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
             y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-        })(window, document, "clarity", "script", ${process.env.NEXT_PUBLIC_CLARITY_TOKEN});
+        })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_TOKEN}");
           `,
         }}
       />
