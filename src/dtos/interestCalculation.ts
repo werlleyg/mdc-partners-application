c855d5865402ac252262742a96e5b58e47fb86ca
Werlley Gonçalves <werlley.registros@hotmail.com>
2023-07-28 21:18:54 -0300

    feat: create IInterestCalculationData interface

---
+++ src/dtos/interestCalculation.ts
@@ -0,0 +1,8 @@
+export interface IInterestCalculationData {
+  initialInvestment?: string;
+  interestRate?: string;
+  typeOfInterest?: string;
+  investmentMonths?: string;
+  showResult?: boolean;
+  investmentAmount?: string;
+}
