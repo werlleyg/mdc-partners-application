559e329930d2023c71594b722922d952c3c96650
Werlley Gonçalves <werlley.registros@hotmail.com>
2023-07-28 23:54:32 -0300

    hotfix: change vowelcounter name

--- src/pages/vowelcounter/index.tsx
+++ src/pages/vowelcounter/index.tsx
@@ -11,7 +11,7 @@ import { Header } from "@/components/Header";
 import { DotSpinner } from "@/components/DotSpinner";
 
 // types
-import { IVowelCounterData } from "@/dtos/vowelCounter";
+import { IVowelCounterData } from "@/dtos/vowelcounter";
 
 // utils
 import { removeAccentVowels } from "@/utils/removeAccentVowels";
