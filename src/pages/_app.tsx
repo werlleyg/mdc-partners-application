af193c05dc584d404c9c4bea50763557fd0ac686
Werlley Gonçalves <werlley.registros@hotmail.com>
2023-07-29 02:05:38 -0300

    feat: add clarity code in app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { CustomFooter } from "@/layout/CustomFooter";
+import Script from "next/script";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
@@ -23,6 +24,20 @@ export default function App({ Component, pageProps }: AppProps) {
         draggable
         pauseOnHover
         theme="light"
+      />
+      {/* clarity code   */}
+      <Script
+        strategy="afterInteractive"
+        id="my_clarity_code"
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", ${process.env.NEXT_PUBLIC_CLARITY_TOKEN});
+          `,
+        }}
       />
     </>
   );
